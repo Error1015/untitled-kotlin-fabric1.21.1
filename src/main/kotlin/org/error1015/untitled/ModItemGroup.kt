31fae88fae68e9eb25888cd320e8326be32085be
liang <liangliang_1015@qq.com>
2024-12-15 16:40:56 +0800

    自定义物品栏

--- src/main/kotlin/org/error1015/untitled/ModItemGroup.kt
+++ src/main/kotlin/org/error1015/untitled/ModItemGroup.kt
@@ -6,6 +6,20 @@ import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents.ModifyEntries
 import net.minecraft.item.ItemGroups
 import org.error1015.untitled.item.ModItems
 
+object ModItemGroup {
+    // 自定义物品栏
+    private val ModGroup: ItemGroup = Registry.register(
+        Registries.ITEM_GROUP,
+        Identifier.of(Untitled.MODID, "example_mod_group"),
+        FabricItemGroup.builder()
+            .displayName(Text.translatable("itemGroup.untitled.example_mod_group"))
+            .icon { ModItems.CUSTOM_ITEM.defaultStack }
+            .entries { _, entries ->
+                entries.add(ModItems.CUSTOM_ITEM)
+                entries.add(ModItems.COOL_ITEM)
+            }
+            .build()
+    )
 
 object CreativeTab {
     private fun addItemToCreativeTab() {
